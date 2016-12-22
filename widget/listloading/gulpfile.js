3acf8a47ecb6b4b449c45454be7cfc521bc4898c
gtdalp <295194117@qq.com>
2016-12-22 11:00:03 +0800

    update 1.1.6

--- widget/listloading/gulpfile.js
+++ widget/listloading/gulpfile.js
@@ -1,26 +1,26 @@
-
 var gulp      = require('gulp');
-// 压缩js
 var uglify    = require('gulp-uglify');
-// 压缩css
 var minifyCss = require('gulp-minify-css');
-// 重命名
 var rename    = require('gulp-rename');
-// 删除
 var del       = require('del');
-// 检查语法
 var eslint    = require('gulp-eslint');
 
 // 清空目录
 gulp.task('clear', function () {
-    // 清空目录
-    del(['./dist/js']);
+    del(['./dist']);
 });
 
-// 压缩
-gulp.task('build', ['clear'], function () {
+// 压缩js
+gulp.task('build', ['build-minifyCss'], function () {
     gulp.src(['./build/*.js'])
         .pipe(uglify())
-        .pipe(rename('focusmap.min.js'))
+        .pipe(rename('listloading.min.js'))
         .pipe(gulp.dest('./dist/js/'));
+});
+// 压缩css
+gulp.task('build-minifyCss', function () {
+    gulp.src(['./demos/*.css'])
+        .pipe(minifyCss())
+        .pipe(rename('listloading.min.css'))
+        .pipe(gulp.dest('./dist/css/'));
 });
