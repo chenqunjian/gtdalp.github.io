95dc39c7da2629ee99bbc648251184a5d9ee94f7
gtdalp <295194117@qq.com>
2017-08-17 18:33:33 +0800

    update

---
+++ web/tongkao/timu.js
@@ -0,0 +1,33 @@
+window.tpl = function (arr, title, o) {
+
+    var html = '';
+    var len = arr.length;
+    var l = len/7;
+
+    for (var i = 0; i < 50; i++) {
+        html += '<li>';
+        var n = 7*i;
+        var t = arr[n];
+        var answ = arr[n+1];
+        var flg = arr[n+6]
+        var cls = (o && o.show) ? ' class="active" ' : '';
+        var datatrue = ' data-true="1"' + cls;
+
+
+        html += '<p class="title">' + t + '</p><p class="answ">' + answ + '</p>';
+        html += '<p class="selected">\
+            <span' + (flg == '答案：A' ? datatrue : '') + '>' + arr[n+2] + '</span>\
+            <span' + (flg == '答案：B' ? datatrue : '') + '>' + arr[n+3] + '</span>\
+            <span' + (flg == '答案：C' ? datatrue : '') + '>' + arr[n+4] + '</span>\
+            <span' + (flg == '答案：D' ? datatrue : '') + '>' + arr[n+5] + '</span>\
+        </p>';
+        html += '</li>';
+    }
+
+    document.title = title;
+    $('body').append('<h3 class="title-h3">' + title + '</h3><ul class="radio-ul"></ul>');
+    $('.radio-ul').html(html);
+    $('.radio-ul>li>p>span').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+}
